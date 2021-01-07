c99bd738af360ad0c7f21e50129829d38a0b5e22
Hiteshi Singla <72081322+Hiteshi-jscoder@users.noreply.github.com>
2021-01-07 19:56:47 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -2,7 +2,7 @@ class Paper{
     constructor(x,y,radius){
         var options = {
             'isStatic':false,
-            'restitution':0.8,
+            'restitution':0.3,
             'friction':1.0,
             'density':1.0
         }
